2ce81e380b8d4fc9698f70b7b87695a59e3f052b
Noble Mungu <mungunoble@gmail.com>
2020-09-13 22:23:13 +0300

    configure the app

---
+++ config.py
@@ -0,0 +1,23 @@
+class Config:
+    '''
+    Configuration parent class
+    '''
+   
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Developement configuration child class
+    '''
+    pass
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
