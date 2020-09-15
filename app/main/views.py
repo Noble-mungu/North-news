5698179c5a488fd6b71caa2419d0eb5183657d98
Noble Mungu <mungunoble@gmail.com>
2020-09-15 12:35:59 +0300

    add macro to implement the DRY pricnciple

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,12 @@
-views.py
+from flask import render_template
+from .import main
+
+@main.route('/')
+def index():
+	'''
+	Return index page and its data
+	'''
+
+	# news_headlines = get_headlines()
+	title = 'New Highlight- Fast and reliable way to get news'
+	return render_template('index.html',title = title, headlines = news_headlines)
